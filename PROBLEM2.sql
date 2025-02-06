df110a0842f74deb56416dd8327ec0faccc52956
brwmercer <brwmercer@gmail.com>
2025-02-06 09:00:34 -0330

    Add Problem 2 Data

--- PROBLEM2.sql
+++ PROBLEM2.sql
@@ -24,3 +24,37 @@ CREATE TABLE order_items (
     quantity INT,
     PRIMARY KEY (order_id, product_id)
 );
+
+-- Add Data to the file
+
+INSERT INTO products (product_name, price, stock_quantity) VALUES
+('Hockey Stick', 229.99, 35),
+('Hockey Gloves', 99.99, 63),
+('Skates', 149.99, 86),
+('Helmet', 74.99, 104),
+('Jersey', 249.99, 18);
+
+INSERT INTO customers (first_name, last_name, email) VALUES 
+('Connor', 'Mcdavid', 'mvp@nhl.com'),
+('Nathan', 'Mackinnon', 'stud@nhl.com'),
+('Lane', 'Hutson', 'roty@nhl.com'),
+('Sidney', 'Crosby', 'thebest@nhl.com');
+
+INSERT INTO orders (customer_id, order_date) VALUES
+(1, '2025-02-06'),
+(2, '2025-02-06'),
+(3, '2025-02-06'),
+(4, '2025-02-06'),
+(3, '2025-02-06');
+
+INSERT INTO order_items (order_id, product_id, quantity) VALUES
+(1, 1, 1), 
+(1, 2, 1),
+(2, 3, 2),
+(2, 4, 1),
+(3, 1, 3),
+(3, 2, 1),
+(4, 4, 1),
+(4, 2, 1),
+(5, 3, 1),
+(5, 2, 1);
