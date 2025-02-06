fe48741a7c0ae6f6c9b41af861b8a2138b08cb35
brwmercer <brwmercer@gmail.com>
2025-02-06 08:46:31 -0330

    Create Problem 2 Tables

---
+++ PROBLEM2.sql
@@ -0,0 +1,26 @@
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    product_name VARCHAR(50),
+    price DECIMAL (10, 2),
+    stock_quantity INT
+);
+
+CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(50)
+);
+
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    customer_id INT REFERENCES customers(id),
+    order_date DATE
+);
+
+CREATE TABLE order_items (
+    order_id INT REFERENCES orders(id),
+    product_id INT REFERENCES products(id),
+    quantity INT,
+    PRIMARY KEY (order_id, product_id)
+);
