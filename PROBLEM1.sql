3a331a07ff1b4869f9fe3ed6753199bccea105d4
brwmercer <brwmercer@gmail.com>
2025-02-05 20:53:54 -0330

    Create Problem 1 Tables

---
+++ PROBLEM1.sql
@@ -0,0 +1,28 @@
+CREATE TABLE students (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(50),
+    school_enrollment_date DATE
+);
+
+CREATE TABLE professors (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    department VARCHAR(50)
+);
+
+CREATE TABLE courses (
+    id SERIAL PRIMARY KEY,
+    course_name VARCHAR(50),
+    course_description TEXT,
+    professor_id INT REFERENCES professors(id)
+);
+
+CREATE TABLE enrollments (
+    student_id INT REFERENCES students(id),
+    course_id INT REFERENCES courses(id),
+    enrollment_date DATE,
+    PRIMARY KEY (student_id, course_id)
+);
